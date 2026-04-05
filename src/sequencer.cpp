f54c223f742305493cbf43258153493fd08886f1
Aditya Hegde <hegde39@purdue.edu>
2026-04-05 17:03:15 -0400

    hello anant.

--- src/sequencer.cpp
+++ src/sequencer.cpp
@@ -4,14 +4,17 @@ int offset = 0;
 int step = 493.883*N / RATE * (1<<16);
 uint8_t buffer[1024]; 
 unordered_map<int, Track> mp;
+int buffIndex = 0;
 
 
 int main(int argc, char** argv){
     //checks for bs
 
     for(;;){
-        //when keypressed trigger interrupt and do{
+        //when input from anant do{
         Track curr = new Track();
+        //buffIndex += sizeof(curr);
+
 
 
 
