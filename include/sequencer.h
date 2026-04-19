735f0fe0677967d31c27623de2108be3887670cd
Aditya Hegde <hegde39@purdue.edu>
2026-04-19 19:12:37 -0400

    updated

--- include/sequencer.h
+++ include/sequencer.h
@@ -20,7 +20,6 @@ typedef enum {
 
 void sequencer_init();
 void sequencer_set_mode(sequencer_mode_t m);
-sequencer_mode_t sequencer_get_mode();
 void record(note_t n, uint8_t octave, uint8_t channel);
 void sequencer_next();
-uint32_t sequencer_process(uint32_t samp);
+void sequencer_process();
