46dfb6aa156c7ed744a564a138c509f01a507b37
PrjKum <Kumar755@purdue.edu>
2026-04-19 19:40:43 -0400

    changed drum mahcine

--- src/queue.c
+++ src/queue.c
@@ -1,19 +1,22 @@
 #include "queue.h"
 
-KeyEvents kev = { .q = {0}, .head = 0, .tail = 0 };
+KeyEvents kev = {.q = {0},.head = 0,.tail = 0 };
 
-uint16_t key_pop() {
+uint16_t key_pop()
+{
     // Queue is empty
-    while (kev.head == kev.tail) {
-        sleep_ms(10);   // Wait for an event to be pushed
+    while(kev.head == kev.tail)
+    {
+        sleep_ms(10);
     }
     uint16_t value = kev.q[kev.tail];
-    kev.tail = (kev.tail + 1) % 32;
+    kev.tail =(kev.tail + 1) % 32;
     return value;
 }
-void key_push(uint16_t value) {
-    // Queue is full, drop new keys
-    if ((kev.head + 1) % 32 == kev.tail) {
+void key_push(uint16_t value)
+{
+    if((kev.head + 1) % 32 ==kev.tail)
+    {
         return; 
     }
     kev.q[kev.head] = value;
